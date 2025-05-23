e48c6b6dd63e219fd34ee4267372f55d1b3dd382
joshua <Joshua.tite@me.com>
2025-05-23 14:15:32 +0100

    Initial backend push

---
+++ db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2"); // Import the mysql2 module    
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database.');
+});
+
+module.exports = connection;
