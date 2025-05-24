0154e52249829c1199ac766cee9c761b534f8f74
joshua <76907391+ZombiesmasherJT@users.noreply.github.com>
2025-05-24 19:34:37 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2"); // Import the mysql2 module
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME // use your actual variable here if different
 });
 
 connection.connect((err) => {
