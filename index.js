f871d89643cf4f98e393fa288fc3bb2a7cacac99
Joshua Tite <76907391+ZombiesmasherJT@users.noreply.github.com>
2025-05-25 18:05:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const db = require("./db");  // Import db.js
 const authRoutes = require("./routes/auth");
 const authMiddleware = require("./middleware/authMiddleware");
 
+const corsOptions = {
+  origin: "https://wanderstay-fb3lrxzg-zombiesmasherjts-projects.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+
+
 const app = express();
-app.use(cors);
+app.use(cors(corsOptions));     
+app.options("*", cors(corsOptions));  
 app.use(express.json());
 
 // Connect auth routes
